3176c1a1eab934aa6b19ab289e75abe52b34aa07
76836 <74463275+76836@users.noreply.github.com>
2026-09-06 19:42:47 +0000

    1.2.0: cache splash assets and bump service worker to v1.2.0

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE_NAME = 'VerbatimFx-v3-stability';
+const CACHE_NAME = 'VerbatimFx-v1.2.0';
 const ASSETS = [
     './',
     './index.html',
     './css/studio.css',
     './css/icemorphic.css',
+    './css/splash.css',
     './js/main.js',
     './js/ux.js',
     './js/stability.js',
+    './js/splash.js',
     './js/audio-manager.js',
     './js/midi-synth.js',
     './js/midi-recorder.js',
@@ -18,16 +20,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    )).then(() => self.clients.claim()));
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        )).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
 });
